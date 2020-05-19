1fcfad1b927a3eca49bdf0a6e5e0a5349529c3b1
Diana Groza <groza.diana99@gmail.com>
2020-05-19 22:14:13 +0300

    Login class added

---
+++ src/test/ClassTest/LoginTest.java
@@ -0,0 +1,8 @@
+package Account;
+
+public class LoginTest {
+    public static void main(String[] args)
+    {
+        (new Login()).setVisible(true);
+    }
+}
---
+++ src/test/ClassTest/NewUserTest.java
@@ -0,0 +1,8 @@
+import Account.NewUser;
+
+public class NewUserTest {
+    public static void main(String[] args)
+    {
+        NewUser nw = new NewUser();
+    }
+}
