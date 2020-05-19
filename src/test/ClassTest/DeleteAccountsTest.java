21c55c737860c24cdb7dbd527e1498eca0938525
Diana Groza <groza.diana99@gmail.com>
2020-05-19 20:38:51 +0300

    Test for deleting inactive accounts

---
+++ src/test/ClassTest/DeleteAccountsTest.java
@@ -0,0 +1,8 @@
+import Account.DeleteInactiveAccounts;
+
+public class DeleteAccountsTest {
+    public static void main(String[] args)
+    {
+        DeleteInactiveAccounts account = new DeleteInactiveAccounts();
+    }
+}
